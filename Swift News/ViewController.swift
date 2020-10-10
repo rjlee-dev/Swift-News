f1c5e53239dfaa2d81b31aa2ecae1f38d27f9a08
Richard <atomicflare@hotmail.com>
2020-10-10 16:21:15 -0400

    Initial Commit

---
+++ Swift News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift News
+//
+//  Created by Atomicflare on 2020-10-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
